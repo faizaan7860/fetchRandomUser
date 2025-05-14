2d1923f11794d08a6468833331b18f2d578152f7
Faizan <faizangandhi11@gmail.com>
2025-05-14 16:01:21 +0530

    Initial commit: Random User Search project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import RandomUser from './RandomUser';
+import React, { useState } from "react";
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState("")
+
+
+  const users = [
+    { id: 1, name: "Faizan", age: 21, country: "Turkey" },
+    { id: 2, name: "Zaid", age: 22, country: "Morocco" },
+    { id: 3, name: "Arbaaz", age: 23, country: "Dubai" },
+  ];
+
+  const filteredUsers = users.filter((users) =>
+    users.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
   return (
+    
     <div>
-      <h1>Random User Generator</h1>
-      <RandomUser />
+     <h1>Random User Generator</h1>
+
+     <input type="text" placeholder="Search by name..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>   
+
+     {filteredUsers.map((users)=>(
+     <div key={users.id}><h3>{users.name}</h3>
+     <p>Age: {users.age}</p>
+    <p>Country: {users.country}</p>
+    </div> 
+     ))}  
     </div>
   );
 }
